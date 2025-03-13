c1cd011e6b9edda863544c8f8c72e14f2c3b1609
perezadria00 <perezadria00@gmail.com>
2025-03-13 12:49:00 +0100

    Create a new route (inside group actorout) as countActors

--- routes/web.php
+++ routes/web.php
@@ -37,5 +37,6 @@ Route::middleware('year')->group(function () {
     Route::prefix('actorout')->group(function () {
         Route::get('actors', [ActorController::class, 'listActors'])->name('listActors');
         Route::get('listActorsByDecade', [ActorController::class, 'listActorsByDecade'])->name('listActorsByDecade');
+        Route::get('countActors');
     });
 });
